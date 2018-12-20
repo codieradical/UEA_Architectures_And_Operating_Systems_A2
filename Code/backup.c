7d2b217e4fe34ea988652c30b93510f06c02fb5e
Alex <git@alexnewark.co.uk>
2018-12-20 01:24:17 +0000

    Task 3: WIP Backup

--- Code/backup.c
+++ Code/backup.c
@@ -38,41 +38,110 @@
    
    If for any reason this isn't set, the default of 0 will cause the full
    path to be printed, which isn't the end of the world :) */
-static short lengthOfBackupPath = 0;
+static short backupPathLength = 0;
 static time_t modifiedAfterTimestamp = 0;
+static FILE *archiveFile;
 
-/* These are optional due to the order of functions,
-   but I've added them in case I move things around, or call functions more. 
-   Alternatively I could use a header. */
-static int printFile(const char* path, const struct stat *fileStat, 
+/* Structure / Function Definitions
+   Alternatively I could use a header, but the assignment brief only
+   specifies one file name, so I'm sticking to that. 
+   When structures are defined, their body much be fully defined before they
+   are refenced. So I'm placing them at the top. */
+
+/* Tar files are formed of 512 byte blocks. 
+   Before each file in the archive, there's a header block, containg file 
+   details. Following the header is the file's data, with padding to fill the 
+   gap between the end of file, and the next 512 byte block. 
+   
+   There are different tar header formats, I am using the UStar format.
+   Early formats only allowed file paths 100 characters long, maximum.
+   However, UStar format provides an extra 155 characters at the end of the 
+   header for longer files paths, which I am making use of. 
+   The extra characters, if not null, are appended to the beginning of the 
+   first filePath field. */
+struct tar_header_block {
+      /* Old, Pre-POSIX.1-1988 fields */
+      /* Must be null terminated unless all 100 characters are used. */
+      char filePath[100];
+      /* Octal mode. */
+      char fileMode[8];
+      /* Octal number in ASCII, null (or space) terminated, zero padded. */
+      char ownerId[8];
+      /* Octal number in ASCII, null (or space) terminated, zero padded. */
+      char groupId[8];
+      char fileSize[12];
+      /* Numeric, octal unix time format. */
+      char modifiedTime[12];
+      /* The checksum is calculated based on the header with an empty 
+         checksum containing space characters. */
+      char checksum[8];
+      /* This field was originally the link type, but with the ustar format,
+         it represents the file type. 
+         eg, normal file/symbolic link/directory... */
+      char type;
+      /* Must be null terminated unless all 100 characters are used. */
+      char linkName[100];
+      /* UStar only fields from here.   */
+      /* Must be null terminated */
+      char ustarMagic[6];
+      /* Always "00" */
+      char version[2];
+      /* Must be null terminated */
+      char ownerName[32];
+      /* Must be null terminated */
+      char groupName[32];
+      /* Magor and minor are for devices, which I'm not using, so for this
+         purpose they can be left null. */
+      char major[8];
+      char minor[8];
+      /* The file path prefix, must be null terminated unless all 155 characters
+         are used. */
+      char filePathPrefix[155];
+      /* The header uses 500/512 bytes of it's block. Some custom formats
+         can make use of the extra 12 bytes, but in generally it's null. 
+         As an improvement, this tool could use some of this space to store
+         a file's change timestamp (not modified date). */
+      char unused[12];
+};
+
+static int backupFile(const char* path, const struct stat *fileStat, 
    int flag, struct FTW* fileTreeWalker);
 void getModeString(mode_t mode, char modeStr[]);
+unsigned int calculate_checksum(struct tar_header_block *tarHeader);
 void printHelp();
+static void makeHeader(const char* relativePath, const struct stat *fileStatus, 
+   struct tar_header_block *tarHeader);
+static void backup(char* backupPath);
+static void restore();
 
 void printHelp() {
    printf("\nbackupfiles\n\n"
-            "Lists files modified after the given datetime.\n"
-            "usage: backupfiles <options> <list directory>\n"
-            "options: \n"
-            "   -t <datetime>\n"
-            "      the datetime to list files after.\n"
-            "      datetime can be provided as a string of format"
-            "      \"YYYY-MM-DD hh:mm:ss\", or as a file path, from which\n"
-            "      the modified date will be read.\n"
-            "      Defaults to 1970-01-01 00:00:00.\n"
-            "   -h\n"
-            "      Displays utility help (this messsge).\n\n");
+         "Lists files modified after the given datetime.\n"
+         "usage: backupfiles (options) -f <archive path> (backup directory)\n"
+         "options: \n"
+         "   -t <datetime>\n"
+         "      the datetime to list files after.\n"
+         "      datetime can be provided as a string of format"
+         "      \"YYYY-MM-DD hh:mm:ss\", or as a file path, from which\n"
+         "      the modified date will be read.\n"
+         "      Defaults to 1970-01-01 00:00:00.\n"
+         "   -h\n"
+         "      Displays utility help (this messsge).\n"
+         "   -f <filename>\n"
+         "      (Required) The name/path of the archive file to"
+         "      backup to / restore from.\n\n");
    exit(1);
 }
 
 int main(int argc, char *argv[])
 {
    if(argc < 2) {
-      printf("Insufficient Arguments. Use -h for help.");
+      printf("Insufficient Arguments. Use -h for help.\n");
       return 1;
    }
 
-   char path[4096];
+   char backupPath[4096];
+   char archivePath[4351];
 
    for(int i = 1; i < argc; i++) {
 
@@ -106,41 +175,77 @@ int main(int argc, char *argv[])
             modifiedAfterTimestamp = timegm(&timestamp);
          }
          i++;
+         continue;
+      }
+
+      else if(strcmp(argv[i], "-f") == 0) {
+         //If -f is provided with no filename...
+         if(argc <= i + 1) {
+            printf("Invalid Arguments: No filename provided.\n");
+            return 1;
+         }
+
+         strcpy(archivePath, argv[i + 1]);
+
+         i++;
+         continue;
       }
       
       else {
-         strcpy(path, argv[i]);
+         strcpy(backupPath, argv[i]);
       }
    }
 
-   if(strlen(path) < 1) {
-      printf("Invalid Arguments: No path provided.\n");
+   backupPathLength = strlen(backupPath) + 1;
+   int archivePathLength = strlen(archivePath);
+
+   if(archivePathLength < 1) {
+      printf("Invalid Arguments: An archive path is required.\n");
       return 1;
    }
 
-   lengthOfBackupPath = (short)strlen(path) + 1;
+   if(backupPathLength > 1) {
+      archiveFile = fopen(archivePath, "wb+");
+      backup(backupPath);
+   } else {
 
+      //read archive
+   }
+
+   fclose(archiveFile);
+   printf("\n");
+
+   return EXIT_SUCCESS;
+}
+
+static void backup(char* backupPath) {
    struct tm *localTime = localtime(&modifiedAfterTimestamp);
    char timestampString[20];
    strftime(timestampString, 20, "%Y-%m-%d %H:%M:%S", localTime);
 
    printf("\nSearching for files in:\n");
-   printf("%s", path);
+   printf("%s", backupPath);
    printf("\nModified after:\n");
    printf("%s", timestampString);
    printf("\n\n");
 
    int nfds;
    nfds = getdtablesize();
-	if (nftw(path, printFile, nfds, FTW_F | FTW_D) != 0) {
+	if (nftw(backupPath, backupFile, nfds, FTW_F | FTW_D) != 0) {
       printf("Fatal Error: Could not find files.\n"
-               "Please check the provided path: \"%s\".\n", path);
-      return 1;
+               "Please check the provided path: \"%s\".\n", backupPath);
+      fclose(archiveFile);
+      exit(1);
    }
+   /* Write two empty blocks to the end of the file. */
+   /* There has to be a better way to do this... */
+   char padding[1024];
+   memset(padding, 0, 1024);
+   fwrite((void*)padding, 1, 1024, archiveFile);
+}
 
-   printf("\n");
+static void restore() {
 
-   return EXIT_SUCCESS;
 }
 
 void getModeString(mode_t mode, char modeStr[]) {
@@ -148,7 +253,7 @@ void getModeString(mode_t mode, char modeStr[]) {
    /* Allocate a character array (a string) for the permissions string.
       Strings are null-terminated. A null character has a value of 0.
       '\0' is a character of value 0.
-      If strings aren't terminated correcvtly, problems can occur.*/
+      If strings aren't terminated correctly, problems can occur.*/
    modeStr = strcpy(modeStr, "----------\0");
 
    /* Currently, no directory modes use this function,
@@ -173,12 +278,19 @@ void getModeString(mode_t mode, char modeStr[]) {
       eg "-rw-r--r--@ 1 alex231  staff  275 16 Dec 17:21 .gitignore" */
 }
 
-static int printFile(const char* path, const struct stat *fileStat, 
+static int backupFile(const char* path, const struct stat *fileStat, 
    int flag, struct FTW* fileTreeWalker) 
 {
-   /* If the file is not a regular file or directory, 
-      continue looping (skip it). */
-   if (!S_ISREG(fileStat->st_mode)) return 0;
+   /* If the file is not a regular file, 
+      continue looping (skip it). 
+      As a future improvement, directories could be included to preserve
+      permission changes.
+      This could be acomplished by backing-up folders made prior to the
+      filter timestamp, if they contain fields modified after.*/
+   if (!S_ISREG(fileStat->st_mode) && !S_ISDIR(fileStat->st_mode)) return 0;
+
+   if(strlen(&path[backupPathLength]) < 1)
+      return 0;
 
    /* If the file modified or changed timestamp is lower than (before) the 
       supplied modified after timestamp, return, don't print it. */
@@ -214,7 +326,144 @@ static int printFile(const char* path, const struct stat *fileStat,
       fileOwner->pw_name, fileGroup->gr_name, 
       fileStat->st_size, 
       dateString, 
-      &path[lengthOfBackupPath]);
+      &path[backupPathLength]);
+
+   FILE *file;
+   char *fileData;
+   long int filelen;
+
+   file = fopen(path, "rb");  // Open the file in binary mode
+   fseek(file, 0, SEEK_END);          // Jump to the end of the file
+   filelen = ftell(file);             // Get the current byte offset in the file
+   rewind(file);                      // Jump back to the beginning of the file
+
+   fileData = (char *)malloc((filelen)*sizeof(char)); // Enough memory for file
+   fread(fileData, filelen, 1, file); // Read in the entire file
+   fclose(file); // Close the file
+
+   struct tar_header_block *tarHeader = malloc(sizeof(struct tar_header_block));
+   makeHeader(&path[backupPathLength], fileStat, tarHeader);
+   fwrite((void*)tarHeader, 1, 512, archiveFile);
+
+   long int debug = ftell(archiveFile);
+   //printf("current offset: %ld", debug);
+   //printf("path: %s", path);
+   //printf("data: %s", fileData);
+   //printf("sizeof data: %lu", sizeof(fileData));
+   //printf("sizeof %lu", sizeof *fileData);
+   while(filelen > 512) {
+      fwrite(fileData, 1, 512, archiveFile);
+      fileData = &fileData[512];
+      filelen -= 512;
+   }
+   if(filelen > 0) {
+      fwrite(fileData, 1, filelen, archiveFile);
+      /* Write padding. */
+      char *padding = malloc(512 - filelen);
+      memset(padding, 0, 512 - filelen);
+      fwrite((void*)padding, 1, 512 - filelen, archiveFile);
+   }
    
    return 0;
+}
+
+static void makeHeader(const char* relativePath, const struct stat *fileStatus, 
+   struct tar_header_block *tarHeader)
+{
+   /* setup ustar magic and checksum empty */
+   strcpy(tarHeader->ustarMagic, "ustar");
+   strcpy(tarHeader->version, "0");
+   memset(tarHeader->checksum, ' ', 8);
+   strcpy(tarHeader->major, "0000000");
+   strcpy(tarHeader->minor, "0000000");
+
+   /* setup filePath and filePath prefix */
+   unsigned int pathLength = strlen(relativePath);
+   if(pathLength > 255) {
+      printf("Unable to backup files, file path \n\"%s\"\n is too long.\n",
+         relativePath);
+      /* This could be handled differently, by returning the error, 
+         but for this purpose exiting is sufficient. */
+      exit(1);
+   } else if (pathLength > 100) {
+      /* This could be a char, as it's never higher than 155. */
+      int prefixLength = pathLength - 100;
+      strcpy(tarHeader->filePath, &relativePath[prefixLength]);
+      strncpy(tarHeader->filePathPrefix, relativePath, prefixLength);
+      if (prefixLength != 155) {
+         /* null terminate path prefix */
+         tarHeader->filePathPrefix[prefixLength] = 0;
+      }
+   } else {
+      strcpy(tarHeader->filePath, relativePath);
+      if(pathLength != 100) {
+         /* null terminate path */
+         tarHeader->filePathPrefix[pathLength] = 0;
+      }
+   }
+
+   if (S_ISREG(fileStatus->st_mode)) {
+      tarHeader->type = '0';
+   } else if(S_ISDIR(fileStatus->st_mode)) {
+      tarHeader->type = '5';
+   } else {
+      printf("Fatal Error: Unable to create archive header for file "
+            "\n\"%s\"\n , unsupported file type\n", relativePath);
+      /* This could be handled differently, by returning the error, 
+         but for this purpose exiting is sufficient. */
+      exit(1);
+   }
+   
+   sprintf(tarHeader->fileMode, "%06o", fileStatus->st_mode & 0777);
+   sprintf(tarHeader->ownerId, "%06o", fileStatus->st_uid);
+   sprintf(tarHeader->groupId, "%06o", fileStatus->st_gid);
+   sprintf(tarHeader-> fileSize, "%011llo", fileStatus->st_size);
+   sprintf(tarHeader->modifiedTime, "%0lo", fileStatus->st_mtime);
+   
+   struct passwd *fileOwner = getpwuid(fileStatus->st_uid);
+   strcpy(tarHeader->ownerName, fileOwner->pw_name);
+
+   struct group *fileGroup = getgrgid(fileStatus->st_gid);
+   strcpy(tarHeader->groupName, fileGroup->gr_name);
+
+   unsigned int checksum = calculate_checksum(tarHeader);
+   sprintf(tarHeader->checksum, "%06o", checksum);
+
+   // printf("%s%s%s%s%s%s%s%c%s%s%s%s%s%s%s%s%s",
+   //    tarHeader->filePath,
+   //    tarHeader->fileMode,
+   //    tarHeader->ownerId,
+   //    tarHeader->groupId,
+   //    tarHeader->fileSize,
+   //    tarHeader->modifiedTime,
+   //    tarHeader->checksum,
+   //    tarHeader->type,
+   //    tarHeader->linkName,
+   //    tarHeader->ustarMagic,
+   //    tarHeader->version,
+   //    tarHeader->ownerName,
+   //    tarHeader->groupName,
+   //    tarHeader->major,
+   //    tarHeader->minor,
+   //    tarHeader->filePathPrefix,
+   //    tarHeader->unused);
+}
+
+unsigned int calculate_checksum(struct tar_header_block *tarHeader)
+{
+   /* Create a copy of the header so that the checksum field can be safely
+      cleared. */
+   struct tar_header_block *testTarHeader 
+      = malloc(sizeof(struct tar_header_block));
+   memcpy(testTarHeader, tarHeader, sizeof(struct tar_header_block));
+   memset(testTarHeader->checksum, ' ', 8);
+   char *tarHeaderBytes = (char*)&testTarHeader;
+
+   unsigned int checksum = 0;
+
+   for (int i = 0; i < 512; i++) {
+      checksum += tarHeaderBytes[i];
+   }
+
+   return checksum;
 }
